5fb90632f2e74cd8861c5c9fd1f828ec4dd55eab
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-10-15 16:21:53 +0300

    #

---
+++ include/forma.h
@@ -0,0 +1,16 @@
+#ifdef __FORMA_H
+#define __FORMA_H
+#include "punct.h"
+
+class Forma {
+    Punct _locatie;
+
+    public :
+        float getPerimetru();
+        float getArie();
+        void  setLocatie(Punct p);
+        Punct getLocatie();
+        Forma(Punct p = Punct(0,0));
+}
+
+#endif
